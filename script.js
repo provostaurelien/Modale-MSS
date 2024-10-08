da40bbb9e7511f6ea8472fd933f910b2ebf2c41b
Provost Aurelien <Provostaurelien@gmail.com>
2024-10-08 17:33:40 +0200

    Rajout action connecté

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 //Définition des constantes globales
 const modal = document.getElementById("modal");
+const buttonMSS = document.getElementById("buttonMSS");
+const buttonConnecter = document.getElementById("buttonConnecter");
 // Fonction pour fermer la modale
 function closeModal() {
     modal.classList.remove("visible"); // Retire la classe pour masquer la modale
@@ -15,7 +17,7 @@ function closeModal() {
   
         
   
-    // Fonction pour ouvrir la modale ou revenir à la page d'accueil de la modale
+    // Fonction pour ouvrir et gèrer la fermeture de la modale 
     function openModal(event) {
       event.preventDefault(); // Empêche le comportement par défaut du lien
       modal.classList.add("visible"); // Ajoute la classe pour affichage fenetre
@@ -38,3 +40,20 @@ const buttonAnnuler = document.getElementById("buttonAnnuler");
 
 
   });
+
+  // Fonction pour passer le bouton en active à brancher sur le statut de l'api
+
+
+
+
+// Ajouter un écouteur d'événement pour le bouton "buttonConnecter"
+buttonConnecter.addEventListener("click", () => {
+    // Ajouter la classe "connecte" au bouton MSS
+    buttonMSS.classList.add("connecte");
+
+    // Changer le texte du bouton MSS en "Connecté"
+    buttonMSS.innerHTML = `<img src="./MSS.svg" alt="Icône"> Connecté`;
+
+    // Fermer la modale (si nécessaire)
+    closeModal();
+});
